86430904141d02551d6c2f18bd87f082b3850ed8
Yerald Mora <ymora@invercasa.com.ni>
2022-05-07 17:45:55 -0600

    Begin the creation of a webclient application for nativoshortener.api
    Application that consumes nativoshortener.api, its uses bootstrap
    css and js cdn, executing the api calls trought a httpclient in a
    razor Page that recieves a javascript request to a razorpage handler,
    this to avoid reload the page.

---
+++ nativoshortener.web/Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nativoshortener.web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly string _apiUrl;
+
+        public IndexModel(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _apiUrl = configuration["apiurl"];
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostShortenUrlAsync([FromBody] string url)
+        {
+            string json = JsonConvert.SerializeObject(url);
+            
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri($"{_apiUrl}/shortener/shortenlink"),
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+
+            using (var httpClient = _httpClientFactory.CreateClient())
+            {
+                var response = await httpClient.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                
+                return new JsonResult($"{Request.Headers["Referer"]}{responseBody}");
+            }
+
+        }
+    }
+}
---
+++ nativoshortener.web/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+addEvents();
+
+function addEvents() {
+    const btnShortenUrl = document.querySelector("#btnShortenUrl");
+
+    btnShortenUrl.addEventListener('click', ShortenUrl);
+}
+
+function ShortenUrl() {
+    const url = document.querySelector("#txtUrl").value;
+
+    fetchShortenUrl(url)
+        .then(s => document.querySelector('#txtShortedUrl').value = s)
+        .catch(ex => alert(ex));
+}
+
+async function fetchShortenUrl(url) {
+    const response = await fetch("/Index?handler=ShortenUrl", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'RequestVerificationToken': document.querySelector("input[name='__RequestVerificationToken']").value
+        },
+        body: JSON.stringify(url)
+    });
+
+    if (!response.ok)
+        throw new Error(`😢 something went wrong...${response.status}`);
+
+    return await response.json();
+}
